14c261c5d020b5ba04f0458ddccd0b561a4ecd92
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-10 17:07:44 +0100

    Added title/header with description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Welcome to Raccoon Retail</h1>
+<script>
+</script>
+
+<div class="@container flex flex-col w-full">
+	<header class="text-center text-raccoon">
+<h1 class="text-4xl md:text-5xl">Welcome to Raccoon Retail</h1>
+	<h4 class="text-xl md:text-2xl">The raccoon shop with some <em>gambling</em></h4>
+	</header>
+</div>
